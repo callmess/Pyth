37c4e5ffbc4dfce24c88269c991f6c0cb20a8e14
sspc <ilxj20@126.com>
2017-02-11 01:03:59 +0800

    add /playground/first.py

---
+++ playground/first.py
@@ -0,0 +1,5 @@
+#print()会依次打印每个字符串，遇到逗号“,”会输出一个空格
+print("hello Python","how old are you now ?")
+
+#可以打印整数，或者计算结果
+print("200+2000=", 200+2000)
---
+++ playground/forin.py
@@ -0,0 +1,8 @@
+a={'a':12,'b':13}
+for k,v in a.items():
+    print(k,v)
+b=[3,6,9,13]
+for v in b:
+    print(v)
+for c in 'asdfgh':
+    print(c)
