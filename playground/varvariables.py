5bbf55c9c39e963a86ee3c089a92793e5e64c651
sspc <ilxj20@126.com>
2017-02-11 01:40:39 +0800

    add variables

---
+++ playground/varvariables.py
@@ -0,0 +1,35 @@
+# print("could you say \" i love you \"! ")
+#
+# #r'' 里面的内容怒不会转义
+# print(r'C:\User\juneday\data\ ')
+# print("what are you doing now? \n "
+#       "so \n ? ")
+#
+# #用'''...'''的格式表示多行内容
+# print('''line1
+# line2
+# line3''')
+#
+# print(True)
+# print(2>9)
+# print(11%3)
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = '''Hello,
+# Lisa!'''
+# print("n=",n)
+# print("f=",f)
+# print("s1=",s1)
+# print("s2=",s2)
+# print("s3=",s3)
+# print("s4=",s4)
+# print("r'Hello, \"Bart\"'")
+# print("你好")
+# print(ord("你"))
+# 251052923320320
+# print(chr(25105))
+print("我的天".encode('utf-8'))
+print("我的天".encode('gbk'))
