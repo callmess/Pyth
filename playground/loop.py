05d256d227ddbf96cdf58ea77589af35b1c3b32e
sspc <ilxj20@126.com>
2017-02-11 01:39:05 +0800

    add loop.py

---
+++ playground/loop.py
@@ -0,0 +1,17 @@
+# languages=['java','python','ruby','swift']
+# for lan in languages:
+#     print(lan)
+# for fen in range(5):
+#     print(fen)
+res={'java':100,'python':200}
+res['c++']=300
+# print(res)
+# print(res.get("python"))
+# if "java"in res:
+#     print(res['java'])
+# print(res)
+# res.pop("java")
+# print(res)
+# print(set([1,2,4]))
+# a=max
+# print(a(1,2,4))
